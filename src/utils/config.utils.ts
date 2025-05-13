6494f465c157f59daf66e66d6899b521c7770b7b
devprotocol1 <dev.protocol1@gmail.com>
2025-05-13 08:32:27 +0530

    clean up code and remove sickle

--- src/utils/config.utils.ts
+++ src/utils/config.utils.ts
@@ -21,25 +21,6 @@ export type AerodromeContract = {
 
 type AerodromeContracts = Record<number, AerodromeContract>;
 
-type ChainContracts = {
-    [key: number]: {
-        UniswapV3Connector?: string;
-        AerodromeRouterConnector?: string;
-        NftFarmStrategy?: string;
-        SickleImplementation?: string;
-        SickleFactory?: string;
-        NftSettingsRegistry?: string;
-        SickleRegistry?: string;
-        ConnectorRegistry?: string;
-        NftTransferLib?: string;
-        NftSettingsLib?: string;
-        FeesLib?: string;
-        TransferLib?: string;
-        SwapLib?: string;
-        NftZapLib?: string;
-    };
-};
-
 export const uniswapContracts: UniswapContracts = {
     84532: {
         factory: "0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24",
@@ -93,45 +74,6 @@ export const aerodromeContracts: AerodromeContracts = {
         votingEscrow: ''
     }
 };
-
-
-export const vfatContracts: ChainContracts = {
-    84532: {
-        UniswapV3Connector: "0x5d6094F3d68d725153d0938ce5b0E4D7815B42A7",
-        AerodromeRouterConnector: "0x43FDB828aD3D705Fc5D25467f078a724fC209c5D",
-        NftFarmStrategy: "0xE174bB384365CBD50BF50E8dEeA7DF8b8808dfac",
-        SickleImplementation: "0x527A36c3C0e66d10664954cd86B156670e6871E0",
-        SickleFactory: "0x62fB598f4a7379Ca36c2d031443F6c97B8F60C3f",
-        NftSettingsRegistry: "0xcec6e003108B15FA31A7F5BD80f91aAab3E565CF",
-        SickleRegistry: "0xFd8E0705EdCc01A142ed0a8e76F036e38B72Bcc3",
-        ConnectorRegistry: "0xdBaE2aA28b83b952f7542F87420897F4e12F1A99",
-        NftTransferLib: "0xa2C3056E4150A9Df0459FEd70Bc735702dC6Cc30",
-        NftSettingsLib: "0x4FDAd64621cd20CCC94164bF81C299ff75346E6a",
-        FeesLib: "0x2c2fC8A1CDa7d853a214e294ea40De03d9fC1d2D",
-        TransferLib: "0x64e72a67eaE17aa771D97eA07e3407b171f33Fe5",
-        SwapLib: "0xd98634607C1FEc0dfB925c64037a675eb17a2fc2",
-        NftZapLib: "0x758a3B49A4Fee14B18CC8dFA5CeB547Acc594f21"
-    },
-    8453: {
-        UniswapV3Connector: "0x5d53c9614C9054E622b1eA35231cEc2049A3a44e",
-        AerodromeRouterConnector: "",
-        NftFarmStrategy: "0x63B7CB79f503cB9BB154DD3bfFC1095edb272df3",
-        SickleImplementation: "0x72a72C80fe211bEaBce5516974fffa2c9aECBb3D",
-        SickleFactory: "0x52FFaccCBC6B6854Dd639D31b524CFe7485C8e67",
-        NftSettingsRegistry: "0x7e8CfE955e6C747FD3Cd34361410d0933558ff16",
-        SickleRegistry: "0xF1Cf2598d89215d15578813aBc04698BB55b8E3F",
-        ConnectorRegistry: "0x7d540FC712004c30A288962abAc7b47A86907734",
-        NftTransferLib: "0x359A5f4AD8A13cfad7b7C9459929AEDba930BBa8",
-        NftSettingsLib: "0x993A70694594B40b6fBa8Bd228111705e5af32fa",
-        FeesLib: "0x39318fea5B43BE55615c8c2fa9Ac3913425a8A74",
-        TransferLib: "0x20fd03a19Fa0c45f54b8Cc2a0781B10CcE6A1936",
-        SwapLib: "0x98dc8aC4a5AcCbc17bBd91E8cFceb9fc2F317802",
-        NftZapLib: "0x335f82f20E33400258a1cd235bEC4D6B8E601796"
-    },
-    56: {
-
-    }
-}
 
 export type RpcUrls = {
     [key: number]: string;
--- src/utils/web3.utils.ts
+++ src/utils/web3.utils.ts
@@ -1,10 +1,9 @@
 import { ethers, AbiCoder, ZeroAddress } from "ethers";
-import { aerodromeContracts, RpcUrls, rpcUrls, subGraphUrls, uniswapContracts, vfatContracts } from "./config.utils";
+import { aerodromeContracts, RpcUrls, rpcUrls, uniswapContracts } from "./config.utils";
 
 import poolFactoryAbi from "../abi/aerodrome/poolFactoy.json"
 import poolAbi from "../abi/aerodrome/pool.json"
 import nfpmAbi from "../abi/nfpm.json"
-import sickleFactoryAbi from "../abi/sickleFactory.json"
 import erc20Abi from "../abi/erc20.json"
 import uniPoolAbi from "../abi/uniPool.json"
 import uniswapFactoryAbi from "../abi/uniswapFactory.json"
@@ -12,14 +11,10 @@ import routerAbi from "../abi/aerodrome/router.json";
 import voterAbi from "../abi/aerodrome/voter.json";
 
 import { fromUnits, toUnits } from "./math.utils";
-import { getTokenDetails } from "./requests.utils";
 
 import { Price, Token } from '@uniswap/sdk-core'
 import { TickMath, nearestUsableTick } from '@uniswap/v3-sdk';
 import JSBI from "jsbi";
-
-import { MAX_UINT_128 } from "./constant.utils";
-import { getUniswapQuote } from "./quote.utils";
 
 type SwapRoute = {
     from: string;
@@ -83,96 +78,6 @@ export const fetchPosition = async (chainId: number, tokenId: number) => {
         console.log(error, "error")
     }
 };
-
-export const fetchNftBalance = async (chainId: number, wallet: string) => {
-    try {
-        if (!isValidChainId(chainId)) {
-            throw new Error(`Invalid chainId: ${chainId}`);
-        }
-
-        const instance = new ethers.Contract(
-            uniswapContracts[chainId].nfpm as string,
-            nfpmAbi,
-            new ethers.JsonRpcProvider(rpcUrls[chainId])
-        );
-        const sickle = await getSickle(chainId, wallet)
-        const totalNft = Number(await instance.balanceOf(sickle))
-        const ids = [...Array(totalNft)].map((_, i) => {
-            return instance.tokenOfOwnerByIndex(sickle, i)
-        });
-
-        let nftIds = await Promise.all(ids)
-        nftIds = nftIds.map((item) => parseInt(item))
-        return nftIds
-    } catch (error) {
-        console.log(error)
-        return []
-    }
-}
-
-export const getSickle = async (chainId: number, wallet: string) => {
-    if (!isValidChainId(chainId)) {
-        throw new Error(`Invalid chainId: ${chainId}`);
-    }
-    const instance = new ethers.Contract(
-        vfatContracts[chainId].SickleFactory as string,
-        sickleFactoryAbi,
-        new ethers.JsonRpcProvider(rpcUrls[chainId])
-    );
-    const sickle = await instance.sickles(wallet)
-    return sickle
-}
-
-export const userDeposits = async (chainId: number, wallet: string) => {
-    try {
-        if (!isValidChainId(chainId)) {
-            throw new Error(`Invalid chainId: ${chainId}`);
-        }
-        const nfts = await fetchNftBalance(chainId, wallet);
-        if (nfts?.length == 0) return []
-
-        const positionPromises = nfts?.map(async (id) => {
-            const position = await fetchPosition(chainId, id);
-            return {
-                nftId: id,
-                position,
-            };
-        });
-
-        const _positions = await Promise.all(positionPromises)
-
-        const tokenFc = getTokenDetails(subGraphUrls[Number(chainId)])
-        const positions = await Promise.all(
-            _positions.map(async (item) => {
-                const _token0 = await tokenFc(item?.position?.token0?.toLowerCase());
-                const _token1 = await tokenFc(item?.position?.token1?.toLowerCase());
-
-                return {
-                    ...item,
-                    token0: _token0.token,
-                    token1: _token1.token,
-                };
-            })
-        );
-
-        return positions
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-export const getPredictedSickle = async (chainId: number, wallet: string) => {
-    if (!isValidChainId(chainId)) {
-        throw new Error(`Invalid chainId: ${chainId}`);
-    }
-    const instance = new ethers.Contract(
-        vfatContracts[chainId].SickleFactory as string,
-        sickleFactoryAbi,
-        new ethers.JsonRpcProvider(rpcUrls[chainId])
-    );
-    const sickle = await instance.predict(wallet)
-    return sickle
-}
 
 //@ts-expect-error skip the warning
 export const approve = async (token: string, signer, spendor: string, amount: number, decimals: number) => {
@@ -371,134 +276,6 @@ export const calculatePricesFromChanges = (
     };
 };
 
-export const getClaimableAmount = async (
-    chainId: number,
-    nftId: number,
-    wallet: string,
-    pool: string,
-    tickLower: number,
-    tickUpper: number,
-    liquidity: number,
-    token0Decimal: number,
-    token1Decimal: number,
-    tokenIn: string,
-    tokenOut: string,
-    fee: number
-) => {
-    if (!isValidChainId(chainId)) {
-        throw new Error(`Invalid chainId: ${chainId}`);
-    }
-
-    const instance = new ethers.Contract(
-        uniswapContracts[chainId].nfpm as string,
-        nfpmAbi,
-        new ethers.JsonRpcProvider(rpcUrls[chainId])
-    );
-
-    const sickle = await getSickle(chainId, wallet)
-    const results = await instance.collect.staticCall(
-        {
-            tokenId: nftId,
-            recipient: sickle,
-            amount0Max: MAX_UINT_128,
-            amount1Max: MAX_UINT_128,
-        }, { from: sickle }
-    )
-
-    const slot0 = await getSlot(chainId, pool)
-
-    const sqrtPriceX96 = slot0.sqrtPriceX96;
-
-    const tickCurrent = slot0.tick;
-
-    // Convert sqrtPriceX96 to float
-    const sqrtPrice = Number(sqrtPriceX96) / (2 ** 96);
-    const sqrtPriceLower = Math.sqrt(1.0001 ** tickLower);
-    const sqrtPriceUpper = Math.sqrt(1.0001 ** tickUpper);
-
-    let amount0 = 0, amount1 = 0;
-
-    if (tickCurrent < tickLower) {
-        amount0 = liquidity * (1 / sqrtPriceLower - 1 / sqrtPriceUpper);
-    } else if (tickCurrent > tickUpper) {
-        amount1 = liquidity * (sqrtPriceUpper - sqrtPriceLower);
-    } else {
-        amount0 = liquidity * (1 / sqrtPrice - 1 / sqrtPriceUpper);
-        amount1 = liquidity * (sqrtPrice - sqrtPriceLower);
-    }
-
-    amount0 = amount0 / 10 ** token0Decimal
-    amount1 = amount1 / 10 ** token1Decimal
-    const result = await getUniswapQuote(chainId, tokenIn, tokenOut, token0Decimal, 1)
-    //@ts-expect-error ignore the warning
-    const quote = parseFloat(result?.quoteDecimals)
-
-    const a0 = amount0 * quote;
-    const a1 = amount1;
-    let swapAmount = 0;
-    let token0 = "";
-    let token1 = "";
-    let receivedAmount = 0;
-    let decimals = 0;
-    let decimals1 = 0;
-
-    if (a0 > a1) {
-        // Swap Token0 into Token1
-        swapAmount = (a0 - a1) / 2;
-        token0 = tokenIn;
-        token1 = tokenOut;
-        decimals = token0Decimal;
-        decimals1 = token1Decimal;
-        // Convert from Token1 terms to Token0
-        swapAmount = swapAmount / quote;
-    } else if (a1 > a0) {
-        // Swap Token1 into Token0
-        swapAmount = (a1 - a0) / 2;
-        token0 = tokenIn;
-        token1 = tokenOut;
-        decimals = token1Decimal;
-        decimals1 = token0Decimal;
-    }
-
-    const result_ = await getUniswapQuote(chainId, token0, token1, decimals, swapAmount)
-    console.log(chainId, token0, token1, typeof (decimals), decimals, typeof (swapAmount), swapAmount)
-    //@ts-expect-error ignore the warning
-    receivedAmount = parseFloat(result_?.quoteDecimals)
-
-    let finalAmount0 = amount0;
-    let finalAmount1 = amount1;
-
-    if (token0 === tokenIn) {
-        finalAmount0 -= swapAmount;
-        finalAmount1 += receivedAmount;
-    } else {
-        finalAmount1 -= swapAmount;
-        finalAmount0 += receivedAmount;
-    }
-
-    return {
-        amount0: results.amount0.toString(),
-        amount1: results.amount1.toString(),
-        removeLp: {
-            amount0: amount0.toFixed(4),
-            amount1: amount1.toFixed(4),
-            sqrtPrice,
-            quote,
-            receivedAmount,
-            //@ts-expect-error ingore the warning
-            route: result_?.route || "",
-            swapAmount: swapAmount.toFixed(5),
-            token0,
-            token1,
-            rawSwapAmount: toUnits(swapAmount.toFixed(5), decimals),
-            rawReceivedAmount: toUnits(receivedAmount.toFixed(6), decimals1),
-            extraData: encodeData(token0, fee, token1),
-            finalAmount0: finalAmount0.toFixed(5),
-            finalAmount1: finalAmount1.toFixed(5),
-
-        }
-    }
-}
 
 export const encodeData = (tokenIn: string, fee: number, tokenOut: string) => {
     const abiCoder = new AbiCoder();
